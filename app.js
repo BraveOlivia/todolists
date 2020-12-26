39e1f0c026192a445ffc1e227783993c1dc19ca6
olivia <zhouxuanolivia@gmail.com>
2020-12-26 19:40:24 +1000

    add gitignore, procfile and updated ports

--- app.js
+++ app.js
@@ -124,6 +124,11 @@ app.get("/:listName", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log("server is running on port 3000...");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function () {
+  console.log("server is running successfully.");
 });
